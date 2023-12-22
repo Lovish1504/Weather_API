6c1b49fba06239917850d09662bf4b120c652edc
Lovish arora <aroralovish900@gmail.com>
2023-12-22 23:54:11 +0530

    Initial commit

---
+++ Weather_API_2.py
@@ -0,0 +1,27 @@
+import requests
+
+def get_weather(api_key, city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"  # Use "imperial" for Fahrenheit
+    }
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['weather'][0]['description']
+        temperature = data['main']['temp']
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+        print(f"Weather: {weather}")
+        print(f"Temperature: {temperature}°C")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind_speed} m/s")
+    else:
+        print("Failed to fetch data")
+
+if __name__ == "__main__":
+    api_key = "44385503e6a61e67a3b141bee9698594"
+    city_name = input("Enter city name: ")
+    get_weather(api_key, city_name)
